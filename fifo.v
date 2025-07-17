d9fda71406f23c7a1f3d1c7b43f6af16c1b88fd4
Abhi191005 <shahabhi1910@gmail.com>
2025-07-17 23:39:09 +0530

    Update fifo.v

--- fifo.v
+++ fifo.v
@@ -1,20 +1,22 @@
 module fifo #(
-    parameter DATA_WIDTH = 8,
-    parameter DEPTH = 16
+    parameter data_width = 32,
+    parameter fifo_depth = 8
 )(
     input wire clk,
     input wire rst,
     input wire wr_en,
     input wire rd_en,
+    input cs,
     input wire [DATA_WIDTH-1:0] din,
     output reg [DATA_WIDTH-1:0] dout,
     output wire full,
     output wire empty
 );
 
-    reg [DATA_WIDTH-1:0] mem [0:DEPTH-1];
-    reg [$clog2(DEPTH):0] wr_ptr = 0;
-    reg [$clog2(DEPTH):0] rd_ptr = 0;
+    localparam fifo_depth_log=$clog2(fifo_depth);
+    reg [data_width-1:0] fifo [0:fifo_depth-1];
+    reg [fifo_depth_log:0] wr_ptr = 0;
+    reg [fifo_depth_log:0] rd_ptr = 0;
     reg [$clog2(DEPTH+1):0] count = 0;
 
     assign full = (count == DEPTH);
